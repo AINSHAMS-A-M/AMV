7ff47cf210b87258a8b8eff839607d8002422ae8
Mohamed <mdhn6832@gmail.com>
2025-04-20 02:41:20 +0200

    add AbdElrhman's array and a quick overview of the capabilities in main

---
+++ src/data_structures.hpp
@@ -0,0 +1,113 @@
+#pragma once
+#include <iostream>
+
+template <typename T>
+struct AMArray
+{
+private:
+    T *_data;
+    size_t _size;
+    size_t _capacity;
+
+public:
+    // called when creating an object
+    AMArray(int initial_capacity = 2)
+    {
+        if (initial_capacity < 0)
+        {
+            throw std::out_of_range("Capacity is smaller than zero");
+        }
+
+        _data = new T[initial_capacity];
+        _size = 0;
+        _capacity = initial_capacity;
+    }
+
+    ~AMArray()
+    {
+        delete[] _data;
+    }
+
+    void push_back(T value)
+    {
+        if (_size == _capacity)
+        {
+            _capacity *= 2;
+
+            int *new_data = new T[_capacity];
+
+            for (size_t i = 0; i < _size; i++)
+            {
+                new_data[i] = _data[i];
+            }
+
+            delete[] _data;
+            _data = new_data;
+        }
+
+        _data[_size++] = value;
+    }
+
+    void pop()
+    {
+        if (_size == 0)
+        {
+            throw std::out_of_range("Index out of bounds");
+        }
+
+        _size--;
+    }
+
+    T &at(size_t index)
+    {
+        if (index >= _size)
+        {
+            throw std::out_of_range("Index out of bounds");
+        }
+        return _data[index];
+    }
+
+    std::size_t size() const { return _size; }
+    std::size_t capacity() const { return _capacity; }
+
+    T &operator[](int index) { return at(index); };
+    const T &operator[](int index) const { return at(index); };
+
+    void operator+=(T val)
+    {
+        push_back(val);
+    }
+
+    void operator=(AMArray val)
+    {
+        auto s = val.size();
+
+        delete[] _data;
+        _size = 0;
+        _capacity = val.capacity();
+
+        for (int i = 0; i < s; ++i)
+        {
+            push_back(val[i]);
+        }
+    }
+
+    /// ITERATOR SETUP - Allows you to use "auto" syntax to loop ///
+    struct Iterator
+    {
+    public:
+        Iterator(T *ptr) : _ptr(ptr) {}
+
+        T &operator*() const { return *_ptr; }
+        T *operator->() const { return _ptr; }
+
+        bool operator==(const Iterator &other) const { return _ptr == other._ptr; }
+        bool operator!=(const Iterator &other) const { return _ptr != other._ptr; }
+
+    private:
+        T *_ptr; // Pointer to the current element
+    };
+
+    Iterator begin() { return Iterator(_data); }
+    Iterator end() { return Iterator(_data + _size); }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <algorithm>
+#include "data_structures.hpp"
 
 int main()
 {
     std::cout << "WELCOME TO AMV\n";
+    AMArray<int> a;
+    a.push_back(2);
+    a.push_back(4);
+    a.push_back(5);
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        std::cout << a[i] << ' ';
+    }
     return 0;
 }
