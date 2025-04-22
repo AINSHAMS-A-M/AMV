fa496ae2e831044d0e5bafb4905b43443817ff3a
Mohamed Ahmed <mohamedsdzep50@gmai.com>
2025-04-22 21:30:50 +0200

    save&load data

--- src/db/data_persistence.cpp
+++ src/db/data_persistence.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "data_structures.hpp"
 #include "services.hpp"
 
@@ -8,7 +9,155 @@ MeshVector<PollOption> pollOptions;
 MeshVector<UserVote> userVotes;
 
 /// @brief Loads data from a CSV file and populates in-memory data structures.
-void load_data() {}
+void load_data()
+{
+    // get cwd
+    std::string path = __FILE__;
+    std::string CWD = path.substr(0, path.find("/db"));
+
+    // load users
+    std::fstream csv_in;
+    csv_in.open(CWD + "/data/users.csv", std::ios::in);
+
+    if (!csv_in.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    while (!csv_in.eof())
+    {
+        User current_user;
+        csv_in >> current_user.id >> current_user.name >> current_user.username >> current_user.hashed_password;
+        users.push_back(current_user);
+    }
+
+    csv_in.close();
+
+    // load polls
+    csv_in.open(CWD + "/data/polls.csv", std::ios::in);
+
+    if (!csv_in.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    while (!csv_in.eof())
+    {
+        Poll current_poll;
+        csv_in >> current_poll.id >> current_poll.name >> current_poll.voter_id >> current_poll.owner_id >> current_poll.created_at;
+        polls.push_back(current_poll);
+    }
+
+    csv_in.close();
+
+    // load pollOptions
+    csv_in.open(CWD + "/data/pollOptions.csv", std::ios::in);
+
+    if (!csv_in.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    while (!csv_in.eof())
+    {
+        PollOption current_poll_option;
+        csv_in >> current_poll_option.id >> current_poll_option.poll_id >> current_poll_option.name >> current_poll_option.description;
+        pollOptions.push_back(current_poll_option);
+    }
+
+    csv_in.close();
+
+    // load UserVotes
+    csv_in.open(CWD + "/data/userVotes.csv", std::ios::in);
+
+    if (!csv_in.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    while (!csv_in.eof())
+    {
+        UserVote current_user_vote;
+        csv_in >> current_user_vote.id >> current_user_vote.user_id >> current_user_vote.poll_id >> current_user_vote.poll_option_id;
+        userVotes.push_back(current_user_vote);
+    }
+
+    csv_in.close();
+}
 
 /// @brief Saves current data to a CSV file for persistent storage.
-void save_data() {}
+void save_data()
+{
+    // get cwd
+    std::string path = __FILE__;
+    std::string CWD = path.substr(0, path.find("/db"));
+
+    // save users
+    std::fstream csv_out;
+    csv_out.open(CWD + "/data/users.csv", std::ios::out);
+
+    if (!csv_out.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    for (User current_user : users)
+    {
+        csv_out << current_user.id << current_user.name << current_user.username << current_user.hashed_password << '\n';
+    }
+
+    csv_out.close();
+
+    // save polls
+    csv_out.open(CWD + "/data/polls.csv", std::ios::out);
+
+    if (!csv_out.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    for (Poll current_poll : polls)
+    {
+        csv_out << current_poll.id << current_poll.name << current_poll.voter_id << current_poll.owner_id << current_poll.created_at;
+    }
+
+    csv_out.close();
+
+    // save pollOptions
+    csv_out.open(CWD + "/data/pollOptions.csv", std::ios::out);
+
+    if (!csv_out.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    for (PollOption current_poll_option : pollOptions)
+    {
+        csv_out << current_poll_option.id << current_poll_option.poll_id << current_poll_option.name << current_poll_option.description;
+    }
+
+    csv_out.close();
+
+    // save UserVotes
+    csv_out.open(CWD + "/data/userVotes.csv", std::ios::out);
+
+    if (!csv_out.is_open())
+    {
+        std::cerr << "Error: Unable to connect to database.";
+        return;
+    }
+
+    for (UserVote current_user_vote : userVotes)
+    {
+        csv_out << current_user_vote.id << current_user_vote.user_id << current_user_vote.poll_id << current_user_vote.poll_option_id;
+    }
+
+    csv_out.close();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,21 @@
 int main()
 {
     std::cout << "WELCOME TO AMV\n";
-    MeshVector<int> a;
-    a.push_back(2);
-    a.push_back(4);
-    a.push_back(5);
-    for (size_t i = 0; i < a.size(); ++i)
-    {
-        std::cout << a[i] << ' ';
-    }
-    for (int x : a)
-    {
-        std::cout << x << " ";
-    }
-
+    // MeshVector<int> a;
+    // a.push_back(2);
+    // a.push_back(4);
+    // a.push_back(5);
+    // for (size_t i = 0; i < a.size(); ++i)
+    // {
+    //     std::cout << a[i] << ' ';
+    // }
+    // for (int x : a)
+    // {
+    //     std::cout << x << " ";
+    // }
+    load_data();
+    std::cout << "DONE\n";
+    // std::cout << users.size() << '\n';
+    // for (auto x : users) std::cout << x.id << ' ' << x.name << ' ' << x.username << ' ' << x.hashed_password << '\n';
     return 0;
 }
